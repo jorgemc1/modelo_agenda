e4635e3ecb047b2606afd864100b9558296033d2
jorgemc1 <jprof6@gmail.com>
2023-06-28 19:12:08 -0300

    Criação da tabela de preços em precos.php

--- paginas/precos.php
+++ paginas/precos.php
@@ -2,8 +2,29 @@
    
    <div class="container s12">
    <!---              --->
-
+   <h3>Preços</h3>
+   <hr>
+    <table>
+        <tr>
+            <th>Código</th><th>Produto</th><th>Preço</th>
+        </tr>
+        <tr>
+            <td>001</td>
+            <td>Lógica de Programação</td>
+            <td>R$ 1800,00</td>
+        </tr>
+        <tr>
+            <td>002</td>
+            <td>HTML CSS e JavaScript</td>
+            <td>R$ 1600,00</td>
+        </tr>
+        <tr>
+            <td>003</td>
+            <td>JavaL</td>
+            <td>R$ 1900,00</td>
+        </tr>
+    </table>
    <!---              --->
-       <h3>Preços</h3>
+      
    </div>
 <?php require_once '../html/footer.php' ?>
